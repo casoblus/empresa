22467fe039f3daea06634a6073bd44c8c59151fa
casoblus <casobrinolusquinos@gmail.com>
2016-01-11 21:08:07 +0100

    Clase principal.

---
+++ empresa.java
@@ -0,0 +1,21 @@
+//
+public class Empresa
+{
+	public static void main( String[] args )
+	{
+		// Crea objeto de clase Departegado
+		Empr empregado = new Empr();
+		// Crea objeto de clase Departamento
+		Depart departamento = new Depart();
+
+
+		// ALTAS
+		empregado.ALTA_E();
+		departamento.ALTA_D();
+
+		
+		// MOSTRAR DATOS
+		empregado.VER_E();
+		departamento.VER_D();
+	}
+}
