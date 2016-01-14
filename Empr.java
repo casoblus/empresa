e600dca3994fea2e54004c354f209e0cb04a26d5
casoblus <casobrinolusquinos@gmail.com>
2016-01-14 01:40:47 +0100

    Modificados métodos VER_E y ALTA_E para trabajar con arrays

--- Empr.java
+++ Empr.java
@@ -8,30 +8,33 @@ public class Empr
 	    salario,
 	    Numde;
 
-	public void ALTA_E()
+	public void ALTA_E( Empr[] empreg ) // recibe un array vacío
 	{
 		Scanner sc = new Scanner( System.in );
-
-		System.out.println();
-		System.out.println( "Novo Empregado" );
-		System.out.println( "--------------" );
 		
-		System.out.println( "Nome: " );
-		nome = sc.nextLine(); 
-		
-		System.out.println( "Idade: " );
-		idade = sc.nextInt(); 
-		
-		System.out.println( "Salario: " );
-		salario = sc.nextInt(); 
-		
-		System.out.println( "Numero de departamento: " );
-		Numde = sc.nextInt(); 
-		
-		System.out.println();
+		for( int i = 0; i < empreg.length; i++ )
+		{
+				  System.out.println();
+				  System.out.println( "Novo Empregado" );
+				  System.out.println( "--------------" );
+				  
+				  System.out.println( "Nome: " );
+				  empreg[i].nome = sc.nextLine(); 
+				  
+				  System.out.println( "Idade: " );
+				  empreg[i].idade = sc.nextInt(); 
+				  
+				  System.out.println( "Salario: " );
+				  empreg[i].salario = sc.nextInt(); 
+				  
+				  System.out.println( "Numero de departamento: " );
+				  empreg[i].Numde = sc.nextInt(); 
+				  
+				  System.out.println();
+		}
 	}
 
-	public void VER_E()
+	public void VER_E( Empr empreg ) // recibe un objeto
 	{
 		System.out.println();
 		
